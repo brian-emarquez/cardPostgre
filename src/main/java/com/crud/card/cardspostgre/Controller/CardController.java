efce795dd3e8dc72e8d33df9739266b1355996b7
BrianMarquez3 <brian3marquez@gmail.com>
2024-02-28 14:15:10 -0500

    Controller

--- src/main/java/com/crud/card/cardspostgre/Controller/CardController.java
+++ src/main/java/com/crud/card/cardspostgre/Controller/CardController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,18 +15,26 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/card")
 @CrossOrigin("*")
+
 public class CardController {
     @Autowired
     private ICardService iCardService;
 
+
+    @PostMapping("/list")
     public ResponseEntity<List<Card>> list(){
         var result = iCardService.findAll();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @PostMapping("/save")
     public ResponseEntity<ServiceResponse>save(@RequestBody Card card){
-        var result = iCardService.findAll();
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        ServiceResponse serviceResponse = new ServiceResponse();
+        int result = iCardService.save(card);
+        if (result ==1) {
+            serviceResponse.setMessage("Item saved with success");
+        }
+        return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
     }
 
 
